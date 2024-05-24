bdee3866d9ac5b6109e9c6896521f83f803df3e6
TochilovDenis <denis-tochilov@yandex.ru>
2024-05-24 17:39:39 +0000

    Дополнено класс Logger:
    - параметр УровеньЛоггирования - level - тип LoggingLevels: INFO - 10, DEBUG - 20
    - сеттер для него
    
    - метод Info(string value) который выводит value в случае если level <= INFO
    - метод Debug(string vavlue) который выводит на экран value в случае еслми level <= DEBUG

--- Logger/logger.h
+++ Logger/logger.h
@@ -1,7 +1,24 @@
 #pragma once
 #include <iostream>
 using namespace std;
+
+enum class LoggingLevel { INFO = 10, DEBUG = 20};
+
 class Logger {
+	LoggingLevel mLevel;
 public:
-    	Logger() {}
+    	Logger(LoggingLevel level) : mLevel(level) {}
+
+	void setLevel(LoggingLevel level) { mLevel = level; }
+	void Info(const string& value) const {
+		if (mLevel <= LoggingLevel::INFO) {
+			cout << "INFO: " << value << endl;
+		}
+	}
+	
+	void Debug(const string& value) const {
+		if (mLevel <= LoggingLevel::DEBUG) {
+			cout << "DEBUG: " << value << std::endl;
+		}
+	}
 };
