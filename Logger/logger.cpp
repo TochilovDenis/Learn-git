8eedfb6c721bcd28fc4f61198c419b5f2464fe9d
TochilovDenis <denis-tochilov@yandex.ru>
2024-05-24 18:19:44 +0000

    change logger.cpp

--- Logger/logger.cpp
+++ Logger/logger.cpp
@@ -3,18 +3,22 @@
 #include "lib-02.h"
 
 int main() {
-    // Logger logger;
-    // cout << "Address of Logger object: " << &logger << endl;
+     // Logger logger;
+     // cout << "Address of Logger object: " << &logger << endl;
     
-    // Lib01 lib01;
-    // lib01.Print(logger);
+     Lib01 lib01;
+     lib01.Print();
 
-    // Lib02 lib02;
-    // lib02.Print(logger);
-    
-    Logger logger(LoggingLevel::DEBUG);
-    logger.Info("This is an info message.");
-    logger.Debug("This is a debug message.");
+     Lib02 lib02;
+     lib02.Print();
 
-    return 0;
+     lib01.LoggerAddress();
+     lib02.LoggerAddress();   
+
+
+     // Logger logger1(LoggingLevel::DEBUG);
+     // logger1.Info("This is an info message.");
+     // logger1.Debug("This is a debug message.");
+
+     return 0;
 }
