44c5a3d2965e445b0984efbb228d2046cb1544ff
TochilovDenis <denis-tochilov@yandex.ru>
2024-05-24 17:41:13 +0000

    Added  Logger logger(LoggingLevel::DEBUG);     logger.Info("This is an info message.");     logger.Debug("This is a debug message.");

--- Logger/logger.cpp
+++ Logger/logger.cpp
@@ -3,13 +3,18 @@
 #include "lib-02.h"
 
 int main() {
-    Logger logger;
+    // Logger logger;
     // cout << "Address of Logger object: " << &logger << endl;
     
-    Lib01 lib01;
-    lib01.Print(logger);
+    // Lib01 lib01;
+    // lib01.Print(logger);
 
-    Lib02 lib02;
-    lib02.Print(logger);
+    // Lib02 lib02;
+    // lib02.Print(logger);
+    
+    Logger logger(LoggingLevel::DEBUG);
+    logger.Info("This is an info message.");
+    logger.Debug("This is a debug message.");
+
     return 0;
 }
