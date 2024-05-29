3d03fede93a1e0e274aba2bc394681a82deb17c0
TochilovDenis <denis-tochilov@yandex.ru>
2024-05-29 16:01:53 +0000

    lib-01.h

--- Logger/lib-01.h
+++ Logger/lib-01.h
@@ -5,12 +5,11 @@ class Lib01 {
 	Logger mLogger;
 public:
 //	Lib01(Logger& logger) : mLogger(logger) {}
-	
+	Lib01(LogginLevel level = INFO) { logger.setLevel(logginlevel);}
 	void Print() {
 		cout << "Это метод Print класса Lib01" << endl;
 		mLogger.Info("Вызван метод Print класса Lib01");}
 	void LoggerAddress() {
 		cout << "Адрес логгера в Lib01: " << &mLogger << endl;
 	}
-
 };
